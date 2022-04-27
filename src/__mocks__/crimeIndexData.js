1a25ebde053538ee92435e4ee52771687672a0ca
liuweiliang <liu.weilia@northeastern.edu>
2022-04-27 11:12:12 -0700

    commit crime index page

---
+++ src/__mocks__/crimeIndexData.js
@@ -0,0 +1,14 @@
+import { v4 as uuid } from 'uuid';
+
+export const crimeIndexData = [
+  {
+    id: uuid(),
+    media: '/static/images/products/product_1.png',
+    title: 'Dropbox',
+  },
+  {
+    id: uuid(),
+    media: '/static/images/products/product_2.png',
+    title: 'Medium Corporation',
+  }
+];
